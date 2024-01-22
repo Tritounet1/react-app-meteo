d3ff703b3e7a0c7280c90fe7f46afe840489f0db
Tritounet <107067429+Tritounet1@users.noreply.github.com>
2024-01-22 09:58:43 +0100

    Premier commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const data = [
+    {
+      id: 1,
+      name: "test1",
+    },
+    {
+      id: 2,
+      name: "test2",
+    },
+    {
+      id: 3,
+      name: "test3",
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      {data.map((value, index) => {
+          return(
+            <div key={index}>
+              <p> name: {value.name} </p>
+            </div>
+          );
+      })}
+
     </div>
   );
+
 }
 
 export default App;
